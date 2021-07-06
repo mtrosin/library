1ac185dd563eafb46b37d3642d896a7c37970327
mtrosin <mtrosin@gmail.com>
2021-07-06 14:38:34 -0300

    Added author data

--- app/migrations/Version20210705235718.php
+++ app/migrations/Version20210705235718.php
@@ -25,6 +25,7 @@ final class Version20210705235718 extends AbstractMigration
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(120) NOT NULL, password VARCHAR(120) NOT NULL, date_add DATETIME NOT NULL, date_mod DATETIME DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE book ADD CONSTRAINT FK_CBE5A33169CCBE9A FOREIGN KEY (author_id_id) REFERENCES author (id)');
         $this->addSql('insert into user(username, password, date_add, roles) values(\'admin\', \'$2y$13$5Vfhg1J.0PI.kOzu/xL0c.Ah9/Qir40QhtZ7QYtqqwDZAb2yNAmNW\', \'2021-07-06 22:00:00\', \'["ROLE_ADMIN"]\')');
+        $this->addSql('insert into author(name) values(\'Autor 1\')');
     }
 
     public function down(Schema $schema): void
