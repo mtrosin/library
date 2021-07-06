40e08841dd499eb2dc96861a36b020085ad1207e
Matheus Rosin <mtrosin@gmail.com>
2021-07-06 01:16:39 -0300

    Building Books CRUD

---
+++ app/src/Controller/BookController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Book;
+use App\Form\BookType;
+use App\Repository\BookRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/book')]
+class BookController extends AbstractController
+{
+    #[Route('/', name: 'book_index', methods: ['GET'])]
+    public function index(BookRepository $bookRepository): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_USER');
+        
+        return $this->render('book/index.html.twig', [
+            'books' => $bookRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'book_new', methods: ['GET', 'POST'])]
+    public function new(Request $request): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_USER');
+
+        $book = new Book();
+        $form = $this->createForm(BookType::class, $book);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($book);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('book_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('book/new.html.twig', [
+            'book' => $book,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'book_show', methods: ['GET'])]
+    public function show(Book $book): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_USER');
+
+        return $this->render('book/show.html.twig', [
+            'book' => $book,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'book_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Book $book): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_USER');
+
+        $form = $this->createForm(BookType::class, $book);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('book_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('book/edit.html.twig', [
+            'book' => $book,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'book_delete', methods: ['POST'])]
+    public function delete(Request $request, Book $book): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_USER');
+        
+        if ($this->isCsrfTokenValid('delete'.$book->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($book);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('book_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
